94a9be5fd418b1f13b81d0e6513432c1a4964594
Kelvin <konahkelvin9@gmail.com>
2025-07-08 13:23:46 +0000

    Tailwind biuld config

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './output.css'
-<link href="./output.css" rel="stylesheet"></link>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
